60100c8febb670ef19a30dfa0cc05752e59f34fb
Vsevolod <vsevolodturkov8@gmail.com>
2022-11-08 00:03:28 +0200

    Научился работать с переменными

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "dffddfdfHello, World!" << std::endl;
+    cout << "Hello" << endl;
+    int oranges, bananas, apples;
+    oranges = 14;
+    bananas = -3;
+    apples = 9;
+    cout <<"Oranges:" << oranges << endl <<"Banfnas:" << bananas << endl << "Apples:" << apples << endl;
+
     return 0;
-
-
-    //fgfgfgfg
 
 }
